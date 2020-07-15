7a688defee6aee4512cc96c05b35ae6fc6f2fabb
lidiane <lidianegomesbarbosa@gmail.com>
2020-07-15 12:06:49 -0300

    Animando like e deslike

--- RecriandoTinder/Controller/CombineViewController.swift
+++ RecriandoTinder/Controller/CombineViewController.swift
@@ -57,12 +57,20 @@ extension CombineViewController{
 extension CombineViewController{
     
     @objc func handlerCard(_ gesture: UIPanGestureRecognizer){
-        if let card = gesture.view{
+        if let card = gesture.view as? CombineCardView {
             let point = gesture.translation(in: view)
             
             card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
             
             let rotationAngle = point.x / view.bounds.width * 0.4
+            
+            if point.x > 0{
+                card.likeImageView.alpha = rotationAngle * 5
+                card.deslikeImageView.alpha = 0
+            }else{
+                card.likeImageView.alpha = 0
+                card.deslikeImageView.alpha = rotationAngle * 5 * -1
+            }
             
             card.transform = CGAffineTransform(rotationAngle: rotationAngle)
             
@@ -70,6 +78,9 @@ extension CombineViewController{
                 UIView.animate(withDuration: 0.2){
                     card.center = self.view.center
                     card.transform = .identity
+                    
+                    card.likeImageView.alpha = 0
+                    card.deslikeImageView.alpha = 0
                 }
             }
         }
